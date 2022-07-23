ecb49bbea44c997344e724855d95022ee81492be
PBBond <97637272+PBBond@users.noreply.github.com>
2022-07-23 06:46:52 -0500

    added auth0 round 1

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginButton from "./LoginButton";
+
+const Home = () => {
+    return (
+        <div className="grid h-screen place-items-center">
+            <LoginButton />
+        </div>
+    );
+};
+
+export default Home;
